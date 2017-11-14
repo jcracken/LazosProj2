2d7a4309876270a071eedb10cab40b263e81e1f3
Saile Daimwood <slymple@gmail.com>
2017-11-13 23:40:03 -0700

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -341,6 +341,52 @@ int main(void) {
 	//}
 	//output.close();
 
+	//make a map now with degree instead of vector<int>
+	std::map<int, int> mapAS_modified;
+	for (auto const& x : mapAS) {
+		mapAS_modified.insert(std::make_pair(x.first, x.second.size()));
+	}
+
+	//All ASs are in with their degrees, time to sort: sortedMapAS = map(Degree, AS number)
+	std::multimap<int, int> sortedMapAS = flip_map(mapAS_modified);
+
+	//Create Clique S, initialize with highest degree AS
+	std::vector<int> s;
+	s.push_back(sortedMapAS.rbegin()->second);
+	bool done = false;
+
+	//Now let's go through sortedMapAS, iterating from the largest degree to the smallest to find all T1
+	for (auto iter = sortedMapAS.rbegin(); iter != sortedMapAS.rend(); ++iter) {
+		if (iter->second == 6939) {
+			continue;
+		}
+		for (int i = 0; i < s.size(); i++) { // loop through s now to see if each element in S is connected to our next AS
+			if (std::find(mapAS.at(s.at(i)).begin(), mapAS.at(s.at(i)).end(), iter->second) != mapAS.at(s.at(i)).end()) { //element contains next one, can continue
+				done = false;
+				continue;
+			}
+			else {
+				done = true;
+				break;
+			}
+		}
+		if (done) {
+			
+		}
+		else {
+			s.push_back(iter->second);
+		}
+	}
+	std::ofstream output;
+	output.open("Tier_One_AS.txt");
+	output << "AS\n";
+
+
+	for (int i = 0; i < s.size(); i++) {
+		output << std::to_string(s.at(i)) + "\n";
+	}
+	output.close();
+
 	//2.4
 	/*for (int i = 0; i < secondAS.size(); i++) {
 		if (secondAS.at(i).getCust().size() > 0) {
