45989ad6cbed7a06e2f1d34a78f5852797f28cb7
Saile Daimwood <slymple@gmail.com>
2017-11-13 18:26:07 -0700

    Tier 1 AS stuff for 2.3 finished up(I think)

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,22 @@
 #include "parser.h"
 #include "AS.h"
 
+// for 2.3, inspiration from stack overflow
+template<typename A, typename B>
+std::pair<B, A> flip_pair(const std::pair<A, B> &p)
+{
+	return std::pair<B, A>(p.second, p.first);
+}
+
+template<typename A, typename B>
+std::multimap<B, A> flip_map(const std::map<A, B> &src)
+{
+	std::multimap<B, A> dst;
+	std::transform(src.begin(), src.end(), std::inserter(dst, dst.begin()),
+		flip_pair<A, B>);
+	return dst;
+}
+
 int main(void) {
 	int transit = 0;
 	int enterprise = 0;
@@ -71,23 +87,23 @@ int main(void) {
 			secondAS.at(k).setConns(std::stoi(second.at(i)));
 		}
 	}
-	for (j = 0; j < secondAS.size(); j++) {
-		/*if (secondAS.at(j).getNum() == 393406) {
-			for (k = 0; k < secondAS.at(j).getConns().size(); k++) {
-				std::cout << secondAS.at(j).getConns().at(k) << std::endl;
-			}
-		}*/
-		/*for (k = 0; k < ipaddr.size(); k = k + 3) {
-			try {
-				if (std::stol(ipaddr.at(k + 2)) == secondAS.at(j).getNum()) {
-					secondAS.at(j).setIP(ipaddr.at(k));
-				}
-			}
-			catch (std::out_of_range& e) {
-				//shrug
-			}
-		}*/
-	}
+	//for (j = 0; j < secondAS.size(); j++) { // ip stuff
+	//	if (secondAS.at(j).getNum() == 393406) {
+	//		for (k = 0; k < secondAS.at(j).getConns().size(); k++) {
+	//			std::cout << secondAS.at(j).getConns().at(k) << std::endl;
+	//		}
+	//	}
+	//	for (k = 0; k < ipaddr.size(); k = k + 3) {
+	//		try {
+	//			if (std::stol(ipaddr.at(k + 2)) == secondAS.at(j).getNum()) {
+	//				secondAS.at(j).setIP(ipaddr.at(k));
+	//			}
+	//		}
+	//		catch (std::out_of_range& e) {
+	//			//shrug
+	//		}
+	//	}
+	//}
 
 	//2.3
 	//degree = connections + customers
@@ -116,29 +132,49 @@ int main(void) {
 			mapAS.at(connection).push_back(origin);
 		}
 	}
-
-	std::ofstream output;
-	output.open("out3.txt");
-	output << "AS\tDegree";
+	//make a map now with degree instead of vector<int>
+	std::map<int, int> mapAS_modified;
 	for (auto const& x : mapAS) {
-		output << std::to_string(x.first) + "\t" + std::to_string(x.second.size()) + "\n";
+		mapAS_modified.insert(std::make_pair(x.first, x.second.size()));
+	}
+
+	//All ASs are in with their degrees, time to sort: sortedMapAS = map(Degree, AS number)
+	std::multimap<int, int> sortedMapAS = flip_map(mapAS_modified);
+
+	//Create Clique S, initialize with highest degree AS
+	std::vector<int> s;
+	s.push_back(sortedMapAS.rbegin()->second);
+	bool done = false;
+
+	//Now let's go through sortedMapAS, iterating from the largest degree to the smallest to find all T1
+	for (auto iter = sortedMapAS.rbegin(); iter != sortedMapAS.rend(); ++iter) {
+		if (iter->second == 6939) {
+			continue;
+		}
+		for (int i = 0; i < s.size(); i++) { // loop through s now to see if each element in S is connected to our next AS
+			if (std::find(mapAS.at(s.at(i)).begin(), mapAS.at(s.at(i)).end(), iter->second) != mapAS.at(s.at(i)).end()) { //element contains next one, can continue
+				continue;
+			}
+			else {
+				done = true;
+				break;
+			}
+		}
+		if (done) {
+			break;
+		}
+		else {
+			s.push_back(iter->second);
+		}
+	}
+	std::ofstream output;
+	output.open("Tier_One_AS.txt");
+	output << "AS\n";
+
+
+	for (int i = 0; i < s.size(); i++) {
+		output << std::to_string(s.at(i)) + "\n";
 	}
 	output.close();
 	return 0;
-}
-
-// for 2.3, inspiration from stack overflow
-template<typename A, typename B>
-std::pair<B, A> flip_pair(const std::pair<A, B> &p)
-{
-	return std::pair<B, A>(p.second, p.first);
-}
-
-template<typename A, typename B>
-std::multimap<B, A> flip_map(const std::map<A, B> &src)
-{
-	std::multimap<B, A> dst;
-	std::transform(src.begin(), src.end(), std::inserter(dst, dst.begin()),
-		flip_pair<A, B>);
-	return dst;
 }
